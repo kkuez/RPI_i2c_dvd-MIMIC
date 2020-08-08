0156a1c0873efd72ee79fa3eb5fb848aa6cf4655
kkuez <doerping@gmx.de>
2020-08-09 00:05:03 +0200

    Setup  repo and parsing of Data.

---
+++ src/main/java/com/Data.java
@@ -0,0 +1,78 @@
+package com;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Data {
+    public final String adress;
+    public final String data;
+    public final ACK ack;
+    public final RW rw;
+
+    public Data(String adress, String data, RW rw) {
+        this.rw = rw;
+        this.adress = adress;
+        this.data = data;
+        ack = ACK.UNKNOWN;
+    }
+
+    public Data(String adress, String data, ACK ack, RW rw) {
+        this.rw = rw;
+        this.adress = adress;
+        this.data = data;
+        this.ack = ack;
+    }
+
+    public static List<Data> parse(InputStream inputStream) {
+        List<Data> dataList = new ArrayList<>();
+        Scanner scanner = new Scanner(inputStream);
+        boolean validLine = false;
+        String line = "";
+        while(scanner.hasNext()) {
+            line = scanner.nextLine();
+            validLine = line.contains("Adress") && (line.contains("READ") || line.contains("WRITE")) && line
+                    .contains("Data");
+            if(validLine) {
+                dataList.add(parse(line));
+            }
+        }
+        return dataList;
+    }
+
+    private static Data parse(String line) {
+        String addr = line.substring(line.indexOf(" ") + 1, line.indexOf(" ") + 3);
+        RW rw = RW.valueOf(line.substring(line.indexOf(addr + ' ') + 3, line.indexOf(',')));
+        String data;
+        if(line.endsWith(" ")){
+            data = line.substring(line.indexOf("Data: ") + 6, line.lastIndexOf(" "));
+        } else {
+
+            data = line.substring(line.indexOf("Data: ") + 6);
+        }
+        return new Data(addr, data, rw);
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuilder().append("Adress: ").append(adress).append(" ").append(rw).append(", Data: ")
+                .append(data).append(" ").toString();
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public ACK getAck() {
+        return ack;
+    }
+
+    public RW getRw() {
+        return rw;
+    }
+}
---
+++ src/main/java/com/RW.java
@@ -0,0 +1,5 @@
+package com;
+
+public enum RW {
+    READ, WRITE
+}
